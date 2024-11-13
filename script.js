949c8d408c68a3f2410a8b2991f205f4008f80d6
HarshitsSri <srivastavh757@gmail.com>
2024-11-13 13:13:00 +0530

    test

---
+++ script.js
@@ -0,0 +1,5 @@
+        function showresult() {
+            var items=document.getElementById("quantity").value;
+            var cost=1999*items;
+            alert("Thank you! You purchased "+items+" items and it total costed Rs"+cost);
+        }
